4ed16493692d3c00f661f7376aa457f3330cc20b
JohnsonZZ <465629989@qq.com>
2016-06-22 13:42:02 +0800

    修改数据库为storehouse

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'   => 'mysql', // 数据库类型
 	'DB_HOST'   => 'localhost', // 服务器地址
-	'DB_NAME'   => 'huachuang', // 数据库名
+	'DB_NAME'   => 'storehouse', // 数据库名
 	'DB_USER'   => 'root', // 用户名
 	'DB_PWD'    => 'zhang123', // 密码
 	'DB_PORT'   => 3306, // 端口
