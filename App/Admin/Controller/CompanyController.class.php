2630330eb0baf9060d34fb7f307cd0e94afd7dd1
zleevan <422234924@qq.com>
2016-07-01 09:05:20 +0800

    9:05融合zz

--- App/Admin/Controller/CompanyController.class.php
+++ App/Admin/Controller/CompanyController.class.php
@@ -4,8 +4,20 @@ use Admin\Controller\ComController;
 header("Content-type:text/html;charset=utf-8");
 class CompanyController extends ComController {
     public function index(){
+		
+		//$li= "select * from hc_sort RIGHT JOIN hc_company ON hc_company.cid = hc_sort.cid;";  //查询公司和分类
+		//$company = $Company ->query($li);
+		//$company = $Company ->field('hc_company.cid,hc_company.company,hc_sort.sort')->join('hc_sort ON hc_company.cid = hc_sort.cid  ','LEFT') -> select();
 		$Company = M('Company');
-		$company = $Company ->join('LEFT JOIN hc_sort ON  hc_company.id=hc_sort.cid ') -> select();
+		$Sort = M('Sort');
+		$list = $Company->select();
+		foreach($list as $key => $val){
+			$sort = $Sort->where('cid='.$val['cid'])->select();
+			foreach($sort as $child){
+				$list[$key]['sort'] .=$child['sort'];
+			}
+		}
+		dump($list);die();
 		$this->assign('company',$company);
 		$this->display();
 	}
@@ -21,21 +33,39 @@ class CompanyController extends ComController {
 	}
 	public function update(){
 		$Company = M('Company');
+		$Sort = M('Sort');
 		$id = I('post.id');
 		$data['company'] = I('post.company');
+		$sort = I('post.sort') ? explode('-',I('post.sort')) : '';
 		if($id){
-			$Company ->create($data);
-			$result = $Company ->where('id ='.$id) ->save();
-			if($result){
+			if($sort){
+				$value['cid'] = $id;
+				foreach($sort_array as $val){
+					$value['sort'] = $val;	
+					$Sort -> create($value,2);
+					$result2 = $Sort ->save(); //修改分类表		
+				}
+			}
+			$result1 = $Company ->where('id ='.$id) ->save($data); //修改公司表
+			if($result1 && $result2){
 				$this->success('修改公司成功', 'index');
 			} else {
 				$this->error('修改公司失败');
 			}		
 		}
 		else{
-			$Company ->create($data);	
-			$result = $Company ->add();	
-			if($result){
+			$Company -> create($data);				
+			$result1 = $Company -> add(); //新增公司表
+			if($sort_array){
+					$companyid['cid'] = $id;
+					$Sort -> create($companyid);						
+				foreach($sort_array as $val){
+					$value['sort'] = $val;
+					$Sort -> create($value);	
+				}
+			}
+			$result2 = $Sort ->where('cid ='.$id) ->add(); //新增分类表			
+			if($result1 && $result2){
 				$this->success('新增公司成功', 'index');
 			} else {
 				$this->error('新增公司失败');
@@ -44,6 +74,17 @@ class CompanyController extends ComController {
 			
 	}
 	public function del(){
+		$Company = M('Company');
+		$id = I('get.id');
+		
+		if($id){
+			$result = $Company -> where('id='.$id) -> delete();	
+			if($result){
+				$this->success('删除公司成功', 'index');
+			} else {
+				$this->error('删除公司失败');
+			}		
+		}
 		
 		
 		
