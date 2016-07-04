d74ca6ce6c051f42a9ab20759d28320ed776af8f
zleevan <422234924@qq.com>
2016-07-04 11:28:34 +0800

    写好公司与分类

--- App/Admin/Controller/CompanyController.class.php
+++ App/Admin/Controller/CompanyController.class.php
@@ -14,11 +14,15 @@ class CompanyController extends ComController {
 		foreach($list as $key => $val){
 			$sort = $Sort->where('cid='.$val['cid'])->select();
 			foreach($sort as $child){
-				$list[$key]['sort'] .=$child['sort'];
+				if($list[$key]['sort'] == ''){									
+					$list[$key]['sort'] =$child['sort'];						
+				}else{					
+					$list[$key]['sort'] .=','.$child['sort'];
+				}
 			}
 		}
-		dump($list);die();
-		$this->assign('company',$company);
+		dump($_SERVER['HTTP_HOST']);
+		$this->assign('list',$list);
 		$this->display();
 	}
 	public function add(){
@@ -26,69 +30,139 @@ class CompanyController extends ComController {
 	}
 	public function edit(){
 		$Company = M('Company');
-		$data['id'] = I('get.id');
+		$Sort = M('Sort');
+		$data['cid'] = I('get.cid');
 		$company = $Company -> where($data) -> find();
+		
+		$sort = $Sort -> where($data) -> select();
+		foreach($sort as $key => $val){
+			if($key == 0){
+				$company['sort'] = $val['sort'];
+				$company['sid'] = $val['sid'];
+				
+			}else{
+				$company['sort'] .= ','.$val['sort'];
+				$company['sid'] .= ','.$val['sid'];
+			}			
+		}
 		$this->assign('company',$company);
 		$this->display();
 	}
 	public function update(){
 		$Company = M('Company');
 		$Sort = M('Sort');
-		$id = I('post.id');
-		$data['company'] = I('post.company');
-		$sort = I('post.sort') ? explode('-',I('post.sort')) : '';
-		if($id){
-			if($sort){
-				$value['cid'] = $id;
-				foreach($sort_array as $val){
-					$value['sort'] = $val;	
-					$Sort -> create($value,2);
-					$result2 = $Sort ->save(); //修改分类表		
-				}
-			}
-			$result1 = $Company ->where('id ='.$id) ->save($data); //修改公司表
-			if($result1 && $result2){
-				$this->success('修改公司成功', 'index');
-			} else {
-				$this->error('修改公司失败');
-			}		
-		}
+		//修改数据   result1:更新   result2:增删
+		$data = I('post.');
+		
+		$list = array();
+		if($data['cid']){		
+			$sid = explode(',',$data['sid']) ;
+			$sort =  explode(',',$data['sort']) ;
+			$sidlen = count($sid);
+			$sortlen = count($sort);
+			if($sidlen == $sortlen){
+				for($i=0 ; $i<= $sortlen-1 ; $i++){
+					$map['sid'] =  $sid[$i];
+					$list['sort'] =  $sort[$i];
+					$result = $Sort -> where($map) -> save($list);		
+					if($result){
+						addlog('修改'.$data['company'].'公司成功');
+						$this->success('修改'.$data['company'].'公司成功', 'index');
+					} else {
+						addlog('修改'.$data['company'].'公司失败');
+						$this->error('修改'.$data['company'].'公司失败');
+					}						
+				}				
+			}else if($sidlen > $sortlen){
+					$j =  $sortlen;
+					for( $i=0 ; $i<= $sortlen -1 ; $i++ ){
+						$map['sid'] =  $sid[$i];
+						$list['sort'] =  $sort[$i];
+						//dump($map['sid'] );dump($list['sort']);
+						$result1 = $Sort -> where($map) -> save($list);		
+						//dump($result);
+					}
+					for( $j ; $j <= $sidlen - 1 ; $j ++ ){				
+						$result2 = $Sort -> where('sid='.$sid[$j]) -> delete();					
+					}
+					if($result1 &&  $result2){		
+						addlog('修改'.$data['company'].'公司成功');
+						$this->success('修改'.$data['company'].'公司成功', 'index');					
+					} else {
+						addlog('修改'.$data['company'].'公司失败');
+						$this->error('修改'.$data['company'].'公司失败');
+					}	
+			}else if($sidlen < $sortlen){	
+					$j = $sidlen ;
+					for( $i=0 ; $i<= $sidlen  -1 ; $i++ ){
+						$map['sid'] =  $sid[$i];
+						$list['sort'] =  $sort[$i];
+						$result1 = $Sort -> where($map) -> data($list) -> save();						
+					}
+					for( $j ; $j <= $sortlen -1 ; $j ++ ){
+						$m['cid'] = $data['cid'];
+						$m['sort'] = $sort[$j]; 
+						$result2 = $Sort -> data($m) -> add();		
+					}
+					if($result1 &&  $result2){
+						addlog('修改'.$data['company'].'公司成功');
+						$this->success('修改'.$data['company'].'公司成功', 'index');
+					} else {
+						addlog('修改'.$data['company'].'公司失败');
+						$this->error('修改'.$data['company'].'公司失败');
+					}	
+			}			
+				
+		}	
+		//添加公司和分类
 		else{
-			$Company -> create($data);				
-			$result1 = $Company -> add(); //新增公司表
-			if($sort_array){
-					$companyid['cid'] = $id;
-					$Sort -> create($companyid);						
-				foreach($sort_array as $val){
-					$value['sort'] = $val;
-					$Sort -> create($value);	
+			$result1 = $Company -> data('company='.$data['company']) -> add();
+			$map['cid'] = $Company -> max('cid');
+			$map['sort'] = $data['sort'];
+			$result2 = $Sort -> data($map) -> add();
+			if($result1){
+				if($result2){
+					addlog('成功添加'.$data['company'].'公司和其分类');
+					$this->success('成功添加'.$data['company'].'公司和其分类', 'index');
+					}
+				else {
+					addlog('成功添加'.$data['company'].'公司,但分类添加失败');
+					$this->success('成功添加'.$data['company'].'公司,但分类添加失败');
 				}
+			} 
+			else{
+				addlog('添加'.$data['company'].'公司失败');
+				$this->error('添加'.$data['company'].'公司失败');
 			}
-			$result2 = $Sort ->where('cid ='.$id) ->add(); //新增分类表			
-			if($result1 && $result2){
-				$this->success('新增公司成功', 'index');
-			} else {
-				$this->error('新增公司失败');
-			}				
-		}
-			
+		}		
 	}
 	public function del(){
 		$Company = M('Company');
-		$id = I('get.id');
-		
-		if($id){
-			$result = $Company -> where('id='.$id) -> delete();	
-			if($result){
-				$this->success('删除公司成功', 'index');
-			} else {
-				$this->error('删除公司失败');
-			}		
+		$Sort = M('Sort');
+		$cid = I('param.cid');	
+		if( is_array($cid) ){
+			$cid = implode(',',$cid);
+			$map['cid']  = array('in',$cid);
+			$result1 = $Company -> where($map) -> delete();
+			$result2 = $Sort -> where($map) -> delete();
+			//$result = $Sort -> where($map) -> delete();					   //暂时没有删除公司分类			
 		}
-		
-		
-		
-		
+		else{
+			$result1 = $Company -> where('cid='.$cid) -> delete();
+			$result2 = $Sort -> where('cid='.$cid) -> delete();
+			//$result = $Sort -> where('cid='.$cid) -> delete();				
+		}
+		if($result1){
+			if($result2){
+			$this->success('成功删除公司及其分类', 'index');
+			}else{
+				addlog("删除公司成功，分类删除失败");
+				$this->success('成功删除公司，但分类删除失败', 'index');
+				}
+		} else {
+			addlog("删除公司及其分类失败");
+			$this->error('删除公司及其分类失败');
+		}		
 	}
 }
 ?>
