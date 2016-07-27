5b7926c98af73677e520746a3a773f2202ab67c6
JohnsonZZ <465629989@qq.com>
2016-07-27 17:36:36 +0800

    创星谷供销平台

--- App/Home/Controller/GoodsController.class.php
+++ App/Home/Controller/GoodsController.class.php
@@ -1,13 +1,18 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class GoodsController extends Controller {
     public function index(){
+        $this->display();
+	}
+	public function preview(){
+		$pid = I('get.pid');
 		$Product = M('Product');
 		$goods = M('Product')
-					->field('pid,product,photo,company')
+					->field('pid,product,photo,brief,content,company')
 					->join('LEFT JOIN hc_company ON hc_product.pid = hc_company.cid')
-					->select();
+					->where('pid='.$pid)
+					->find();
 		$this->assign("goods",$goods);
         $this->display();
 	}
