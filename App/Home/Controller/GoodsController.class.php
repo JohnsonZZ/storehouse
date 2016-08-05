2fc327624fa628c8f8e99a8b79782e2df7dae145
JohnsonZZ <465629989@qq.com>
2016-08-05 17:16:06 +0800

    详情

--- App/Home/Controller/GoodsController.class.php
+++ App/Home/Controller/GoodsController.class.php
@@ -20,7 +20,7 @@ class GoodsController extends Controller {
 		}
 		$map['tid'] = implode(',',$sid);
 		$count = M('Product')->where($map)->count();
-		$Page = new \Think\Page($count, 5); // 实例化分页类 传入总记录数和每页显示的记录数(10)
+		$Page = new \Think\Page($count, 21); // 实例化分页类 传入总记录数和每页显示的记录数(10)
 		$Page->setConfig('prev', '上一页');
 		$Page->setConfig('next', '下一页');
 		$Page->setConfig('header','');
