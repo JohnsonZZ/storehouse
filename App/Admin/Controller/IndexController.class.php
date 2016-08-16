4e8a8f93c6e3b7791b08fb2cca2d4c0f5a3fbf9a
JohnsonZZ <465629989@qq.com>
2016-08-16 11:14:39 +0800

    新浪登录

--- App/Admin/Controller/IndexController.class.php
+++ App/Admin/Controller/IndexController.class.php
@@ -19,8 +19,8 @@ class IndexController extends ComController {
 		empty($type) && $this->error('参数错误');
 
 		//加载ThinkOauth类并实例化一个对象
-		$sns  = new \Org\ThinkSDK\ThinkOauth::getInstance($type);
-
+		$sns  = new \Org\ThinkSDK\ThinkOauth;
+		$sns->getInstance($type);
 		//跳转到授权页面
 		redirect($sns->getRequestCodeURL());
 	}
